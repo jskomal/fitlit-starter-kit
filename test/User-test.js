70e0d245471bef7a699decb2975de99665112d11
Jordan Skomal <jskomal@gmail.com>
2022-02-05 16:40:48 -0700

    Add tests for User properties

--- test/User-test.js
+++ test/User-test.js
@@ -3,24 +3,52 @@ import User from '../src/User'
 
 describe('User', () => {
   beforeEach(() => {
-    const testUser = {
+    const userData = {
       id: 1,
       name: 'Luisa Hane',
       address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
       email: 'Diana.Hayes1@hotmail.com',
       strideLength: 4.3,
       dailyStepGoal: 10000,
-      friends: [16, 4, 8],
+      friends: [16, 4, 8]
     }
 
-    const user = new User(testUser)
+    const user = new User(userData)
   })
 
   it('should be a function', () => {
     expect(User).to.be.a('function')
   })
 
-  it('should instantiate the User class' () => {
+  it('should instantiate the User class', () => {
     expect(user).to.be.an.instanceof(User)
+  })
+
+  it('should have an id', () => {
+    expect(user.id).to.equal(1)
+  })
+
+  it('should have a name', () => {
+    expect(user.name).to.equal('Lusia Hane')
+  })
+
+  it('should have an address', () => {
+    expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697')
+  })
+
+  it('should have an email', () => {
+    expect(user.email).to.equal('Diana.Hayes1@hotmail.com')
+  })
+
+  it('should have a strideLength' () => {
+    expect(user.strideLength).to.equal(4.3)
+  })
+
+  it('should have a dailyStepGoal', () => {
+    expect(user.dailyStepGoal).to.equal(10000)
+  })
+
+  it('should hold an array of friends', () => {
+    expect(user.friends).to.deep.equal([16, 4, 8])
   })
 })
