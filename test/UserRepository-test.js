2552bc1b89156199f7612c8cb43b6cadcc6ebe04
Jordan Skomal <jskomal@gmail.com>
2022-02-05 17:16:30 -0700

    Add beforeEach to UserRepositiory-Test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,10 +1,47 @@
 import { expect } from 'chai'
 import UserRepository from '../src/UserRepository'
+import User from '../src/User'
 
 describe('User Repository', () => {
+  beforeEach(() => {
+    const user1 = new User({
+      id: 1,
+      name: 'Luisa Hane',
+      address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
+      email: 'Diana.Hayes1@hotmail.com',
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [16, 4, 8],
+    })
+    const user2 = new User({
+      id: 2,
+      name: 'Jarvis Considine',
+      address: '30086 Kathryn Port, Ciceroland NE 07273',
+      email: 'Dimitri.Bechtelar11@gmail.com',
+      strideLength: 4.5,
+      dailyStepGoal: 5000,
+      friends: [9, 18, 24, 19],
+    })
+    const user3 = new User({
+      id: 3,
+      name: 'Herminia Witting',
+      address: '85823 Bosco Fork, East Oscarstad MI 85126-5660',
+      email: 'Elwin.Tromp@yahoo.com',
+      strideLength: 4.4,
+      dailyStepGoal: 5000,
+      friends: [19, 11, 42, 33],
+    })
+
+    const userData = [user1, user2, user3]
+  })
+  
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function')
   })
 
-  
+  it('should be an instantiation of the UserRepository class', () => {
+    
+  })
+
+
 })
