4aaa7a30b2af305d1d9a4b2eb1f8d2a6da3bfd56
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-05 18:38:02 -0700

    Add returnUserById method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -2,6 +2,14 @@ class UserRepository {
   constructor(userData) {
     this.data = userData;
   }
+  returnUserById(id){
+    return this.data.find((user) => {
+      return user.id === id
+    })
+  }
+  returnAvgSteps() {
+    
+  }
 }
 
 export default UserRepository;
