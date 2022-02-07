67c590e070b9f5989c9890917d62fd43bcaeac51
Jordan Skomal <jskomal@gmail.com>
2022-02-07 15:39:19 -0700

    Implement fetch requests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,18 @@
-// Your fetch requests will live here!
+const userDataFetch = fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+.then(response => response.json())
+.catch(error => console.log(error))
 
+const userDataSleep = fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
+.then((response) => response.json())
+.catch((error) => console.log(error))
 
-console.log('I will be a fetch request!')
+const userDataActivity = fetch('https://fitlit-api.herokuapp.com/api/v1/activity')
+.then((response) => response.json())
+.catch((error) => console.log(error))
+
+const userDataHydration = fetch('https://fitlit-api.herokuapp.com/api/v1/hydration')
+.then((response) => response.json())
+.catch((error) => console.log(error))
+
+export default { userDataFetch, userDataSleep, userDataActivity, userDataHydration }
 
