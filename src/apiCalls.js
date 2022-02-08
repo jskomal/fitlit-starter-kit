a2c1f07d983d18dc1a78d770d4a0c58b4003a4de
Jessica Fatta <jessicafatta@gmail.com>
2022-02-08 17:36:44 -0500

    Add return statements to fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,21 +1,21 @@
 const fetchUserData = () => {
-  fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/users')
   .then(response => response.json())
   
 }
 
 const fetchSleepData = () => {
-  fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
   .then(response => response.json())
 }
 
 const fetchActivityData = () => {
-  fetch('https://fitlit-api.herokuapp.com/api/v1/activity')
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/activity')
   .then(response => response.json())
 }
 
 const fetchHydrationData = () => {
-  fetch('https://fitlit-api.herokuapp.com/api/v1/hydration')
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/hydration')
   .then(response => response.json())
 }
 
