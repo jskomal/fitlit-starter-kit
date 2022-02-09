6d763dbc71150d9ecb7bc7f4cedcef7f15407096
Jordan Skomal <jskomal@gmail.com>
2022-02-09 10:32:12 -0700

    Refactor tests

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { expect } from 'chai'
 import User from '../src/User'
 import Hydration from '../src/Hydration'
@@ -97,13 +98,13 @@ describe('Hydration', () => {
 
   it('should have an a property that holds the amount of water per day', () => {
     expect(hydratedUser.waterData).to.deep.equal([
-      { '2019/06/15': 85 },
-      { '2019/06/16': 69 },
-      { '2019/06/17': 54 },
-      { '2019/06/18': 41 },
-      { '2019/06/19': 37 },
-      { '2019/06/20': 75 },
-      { '2019/06/21': 47 },
+      { userID: 1, date: dayjs('2019/06/15'), numOunces: 85 },
+      { userID: 1, date: dayjs('2019/06/16'), numOunces: 69 },
+      { userID: 1, date: dayjs('2019/06/17'), numOunces: 54 },
+      { userID: 1, date: dayjs('2019/06/18'), numOunces: 41 },
+      { userID: 1, date: dayjs('2019/06/19'), numOunces: 37 },
+      { userID: 1, date: dayjs('2019/06/20'), numOunces: 75 },
+      { userID: 1, date: dayjs('2019/06/21'), numOunces: 47 },
     ])
   })
 
@@ -115,9 +116,10 @@ describe('Hydration', () => {
     expect(hydratedUser.getWaterByDate('2019/06/21')).to.equal(47)
   })
 
-  it('should have a method to return how many ounce drank per day over the course of a week', () => {
+  it('should have a method to return ounces of water drank per day over the course of a week', () => {
     expect(hydratedUser.getWaterInWeek('2019/06/21')).to.deep.equal([
       85, 69, 54, 41, 37, 75, 47
     ])
   })
 })
+
