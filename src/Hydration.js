98f3aef96fa474aeb3594cce7b346ce975ff4674
Jordan Skomal <jskomal@gmail.com>
2022-02-08 20:05:46 -0700

    Implement getWaterByDate

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,15 +1,21 @@
-
 class Hydration {
   constructor(filteredUserData) {
-    this.userID = filteredUserData[0].userID;
+    this.userID = filteredUserData[0].userID
     this.waterData = filteredUserData.reduce((acc, currentEntry) => {
-      acc.push({[currentEntry.date]:
-        currentEntry.numOunces})
+      acc.push({ [currentEntry.date]: currentEntry.numOunces })
       return acc
     }, [])
   }
+
+  //[{ '2019/06/15': 85 },
+  // { '2019/06/16': 69 },
+  // { '2019/06/22': 54 }]
+
+  getWaterByDate(date) {
+    return this.waterData.find((waterLogEntry) => {
+      return waterLogEntry[date]
+    })[date]
+  }
 }
 
-
-
-export default Hydration;
+export default Hydration
