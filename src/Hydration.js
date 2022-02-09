54295b3a5e1123eea2239ba8e7371dbe3cef1625
Jordan Skomal <jskomal@gmail.com>
2022-02-08 20:20:54 -0700

    Implement getAvgWater

--- src/Hydration.js
+++ src/Hydration.js
@@ -5,11 +5,22 @@ class Hydration {
       acc.push({ [currentEntry.date]: currentEntry.numOunces })
       return acc
     }, [])
+    this.allWaterOz = filteredUserData.reduce((acc, currentEntry) => {
+      acc.push(currentEntry.numOunces)
+      return acc
+    }, [])
   }
 
   //[{ '2019/06/15': 85 },
   // { '2019/06/16': 69 },
   // { '2019/06/22': 54 }]
+
+  getAvgWater() {
+    return parseFloat(((this.allWaterOz.reduce((acc, currentEntry) => {
+      acc += currentEntry
+      return acc
+    }, 0))/this.allWaterOz.length).toFixed(2))
+  }
 
   getWaterByDate(date) {
     return this.waterData.find((waterLogEntry) => {
