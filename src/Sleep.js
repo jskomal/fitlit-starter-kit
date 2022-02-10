b7cb4274e4c977182335f4e017559c70bdfb784d
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-10 16:35:43 -0700

    Add passing id test

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,9 +1,18 @@
 
 class Sleep {
-  constructor() {
-
+  constructor(userID, userSleepData) {
+    this.userID = userID;
   }
 }
 
 
 export default Sleep;
+
+
+// For a user (identified by their userID), the average number of hours slept per day
+// For a user, their average sleep quality per day over all time
+// For a user, how many hours they slept for a specific day (identified by a date)
+// For a user, their sleep quality for a specific day (identified by a date)
+// For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+// For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+// For all users, the average sleep quality
