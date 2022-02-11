fb306f2236e32f4c3707800b98d4eae4f626e096
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-10 17:04:37 -0700

    Add getSleepTimeByDate method

--- src/Sleep.js
+++ src/Sleep.js
@@ -23,14 +23,17 @@ class Sleep {
       return acc + sleepLogEntry.sleepQuality
     },0)/this.sleepData.length).toFixed(2))
   }
+  getSleepTimeByDate(date) {
+    return this.sleepData.find(sleepLogEntry => {
+      return dayjs(date).isSame(sleepLogEntry.date)
+    }).hoursSlept
+  }
 }
 
 
 export default Sleep;
 
 
-// For a user (identified by their userID), the average number of hours slept per day
-// For a user, their average sleep quality per day over all time
 // For a user, how many hours they slept for a specific day (identified by a date)
 // For a user, their sleep quality for a specific day (identified by a date)
 // For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
