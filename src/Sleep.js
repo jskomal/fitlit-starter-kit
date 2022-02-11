9c881cf9d67c230aff45546b849c4d380a9d3fb3
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-10 17:22:48 -0700

    Add refactor

--- src/Sleep.js
+++ src/Sleep.js
@@ -13,7 +13,7 @@ class Sleep {
       }
     })
   }
-  
+
   getAvgSleepTime() {
     return parseFloat((this.sleepData.reduce((acc, sleepLogEntry) => {
       return acc + sleepLogEntry.hoursSlept
@@ -64,6 +64,4 @@ class Sleep {
 
 export default Sleep;
 
-// For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-// For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // For all users, the average sleep quality
