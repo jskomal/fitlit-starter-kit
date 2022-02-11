5869f359e9cbcfc1b62c226dad92d14363697576
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-10 19:50:13 -0700

    Add getAvgAllSleepQuality method to pass test

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -1,6 +1,16 @@
+import dayjs from 'dayjs'
+import isBetween from 'dayjs/plugin/isBetween'
+dayjs.extend(isBetween)
+import Sleep from '../src/Sleep'
+
 class SleepRepository{
   constructor(allSleepData) {
     this.allSleepData = allSleepData;
+  }
+  getAvgAllSleepQuality() {
+    return parseFloat((this.allSleepData.reduce((acc, currentUser) => {
+      return acc + currentUser.getAvgSleepQuality()
+    }, 0)/this.allSleepData.length).toFixed(2))
   }
 }
 
