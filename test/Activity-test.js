ef03a946b2c3110c25b604748a8c64a15fec3548
Jessica Fatta <jessicafatta@gmail.com>
2022-02-22 17:14:26 -0500

    Add first test

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -10,26 +10,27 @@ describe('Activity', () => {
 
   beforeEach(() => {
     userActivityData1 = {
-      userID: 1,
-      date: "2019/06/15",
+      date: '2019/06/15',
       numSteps: 3577,
       minutesActive: 140,
       flightsOfStairs: 16 
     },
     userActivityData2 = {
-      userID: 2,
-      date: "2019/06/15",
+      date: '2019/06/16',
       numSteps: 4294,
       minutesActive: 138,
       flightsOfStairs: 10 
     },
     userActivityData3 = {
-      userID: 3,
-      date: "2019/06/15",
+      date: '2019/06/17',
       numSteps: 7402,
       minutesActive: 116,
       flightsOfStairs: 33 
     } 
+  })
+
+  it('should be a function', () => {
+    expect(Activity).to.be.a('function')
   })
 
 
