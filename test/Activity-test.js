48adfd046e835ae0932299c71bd05ed900cd835d
Jessica Fatta <jessicafatta@gmail.com>
2022-02-22 17:09:49 -0500

    Add before each data

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -10,8 +10,26 @@ describe('Activity', () => {
 
   beforeEach(() => {
     userActivityData1 = {
-      
-    }  
+      userID: 1,
+      date: "2019/06/15",
+      numSteps: 3577,
+      minutesActive: 140,
+      flightsOfStairs: 16 
+    },
+    userActivityData2 = {
+      userID: 2,
+      date: "2019/06/15",
+      numSteps: 4294,
+      minutesActive: 138,
+      flightsOfStairs: 10 
+    },
+    userActivityData3 = {
+      userID: 3,
+      date: "2019/06/15",
+      numSteps: 7402,
+      minutesActive: 116,
+      flightsOfStairs: 33 
+    } 
   })
 
 
