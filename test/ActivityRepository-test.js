8bf844fcf279917f56a9d08c861361a0d2003ff9
Jordan Skomal <jskomal@gmail.com>
2022-02-23 17:11:50 -0700

    Refactor beforeEach and add .only

--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -3,54 +3,54 @@ import Activity from '../src/Activity'
 import ActivityRepository from '../src/ActivityRepository'
 import dayjs from 'dayjs'
 
-describe('Activity Repository', () => {
-  let activityUser1;
-  let activityUser2;
-  let activityUser3;
-
-  let activityRepository
-
+describe.only('Activity Repository', () => {
   let userActivityData1
   let userActivityData2
   let userActivityData3
 
-  let testActivityData
+  let activityUser1
+  let activityUser2
+  let activityUser3
+
+  let activity1
+  let activity2
+  let activity3
 
   let testFilteredData
+  let activityRepository
 
-  beforeEach (() => {
+  beforeEach(() => {
     userActivityData1 = {
       date: '2019/06/15',
       numSteps: 3577,
       minutesActive: 140,
-      flightsOfStairs: 16 
+      flightsOfStairs: 16
     }
+
     userActivityData2 = {
       date: '2019/06/15',
       numSteps: 4294,
       minutesActive: 138,
-      flightsOfStairs: 10 
-    } 
+      flightsOfStairs: 10
+    }
+
     userActivityData3 = {
       date: '2019/06/15',
       numSteps: 7402,
       minutesActive: 116,
-      flightsOfStairs: 33 
-    },
+      flightsOfStairs: 33
+    }
 
-    testActivityData = [{
-      1: [userActivityData1],
-      2: [userActivityData2],
-      3: [userActivityData3]
-    }]
+    activityUser1 = { 1: [userActivityData1] }
+    activityUser2 = { 2: [userActivityData2] }
+    activityUser3 = { 3: [userActivityData3] }
 
+    activity1 = new Activity(1, activityUser1)
+    activity2 = new Activity(2, activityUser2)
+    activity3 = new Activity(3, activityUser3)
 
-    activityUser1 = new Activity(1, userActivityData1)
-    activityUser2 = new Activity(2, userActivityData2)
-    activityUser3 = new Activity(3, userActivityData3)
-    testFilteredData = [activityUser1, activityUser2, activityUser3]
+    testFilteredData = [activity1, activity2, activity3]
     activityRepository = new ActivityRepository(testFilteredData)
-  
   })
   it('should be a function', () => {
     expect(ActivityRepository).to.be.a('function')
@@ -61,7 +61,7 @@ describe('Activity Repository', () => {
   })
 
   it('should hold all the user data', () => {
-    expect(activityRepository.allActivityData).to.deep.equal(testActivityData)
+    expect(activityRepository.allActivityData).to.deep.equal(testFilteredData)
   })
 
   it('should have a method to calculate the average flights of stairs by date', () => {
@@ -76,4 +76,3 @@ describe('Activity Repository', () => {
     expect(activityRepository.getAllAvgMinsActiveByDate('2019/06/15')).to.equal(148)
   })
 })
-
