f379918bb7648c445e919f8e7b351f4a528f5b7f
Jordan Skomal <jskomal@gmail.com>
2022-02-23 17:12:50 -0700

    Impliment AllActivityData

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -3,10 +3,8 @@ import Activity from '../src/Activity'
 
 class ActivityRepository {
   constructor(allActivityData) {
-    
+    this.allActivityData = allActivityData
   }
 }
-
-
 
 export default ActivityRepository
