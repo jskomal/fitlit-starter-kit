80ff1b119e6f2e77d066a711e4c2ca403895dcd0
Jordan Skomal <jskomal@gmail.com>
2022-02-23 17:32:21 -0700

    Implement getAllAvgStairsByDate method

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -5,6 +5,20 @@ class ActivityRepository {
   constructor(allActivityData) {
     this.allActivityData = allActivityData
   }
+  getAllAvgStairsByDate(date) {
+    const allFlightsByDate = []
+    this.allActivityData.forEach((user) => {
+      allFlightsByDate.push(
+        user.activityData.find((activityLogEntry) => {
+          return dayjs(date).isSame(activityLogEntry.date)
+        }).flightsOfStairs
+      )
+    })
+    const sum = allFlightsByDate.reduce((acc, currentFlights) => {
+      return acc + currentFlights
+    }, 0)
+    return parseFloat((sum / this.allActivityData.length).toFixed(2))
+  }
 }
 
 export default ActivityRepository
